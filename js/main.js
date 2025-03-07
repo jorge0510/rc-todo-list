4c8cee2cfa9493cb16a827bef90077d277a01823
Jorge Garcia <jagz0510@live.com>
2025-03-07 12:18:38 -0500

    small changes to css

--- js/main.js
+++ js/main.js
@@ -5,24 +5,48 @@ class todo {
         this.isDone = false;
         this.dateStamp = new Date();
     }
+
+    onComplete() {
+        this.isDone = true;
+    }
 }
 
+const todoArray = [];
+
 document.querySelector('button').addEventListener('click', (e) => {
+
+    console.log(e)
+    //prevent default of form
     e.preventDefault();
+
+    //get input values
     const title = document.querySelector('#title').value;
     const description = document.querySelector('#description').value;
+
+    //create new todo object
     const newTodo = new todo(title, description);
 
+    //select parent of nodeElement
     const ul = document.querySelector('ul');
+
+    //create new nodeElement
     const li = document.createElement('li');
 
-   li.innerHTML = `
-        <input type="radio">
-        <div>
-            <span>${newTodo.dateStamp.toLocaleDateString()}</span>
-            <h5>${newTodo.title}</h5>
-            <p>${newTodo.description}</p>
-        </div>
-    `;
+    li.innerHTML = `
+            <input type="radio">
+            <div>
+                <span>${newTodo.dateStamp.toLocaleDateString()}</span>
+                <h5>${newTodo.title}</h5>
+                <p>${newTodo.description}</p>
+                <button class="precomplete">Completed</button>
+            </div>
+        `;
+
+   todoArray.push(newTodo);
    ul.appendChild(li);
 })
+
+document.getElementsByClassName('precomplete').addEventListener('click', completeTask)
+const completeTask = (e) => {
+    console.log(e.target);
+}
